26099cb856ec5624999056fe8c75475c515b9e6c
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2018-10-04 22:03:05 +0530

    Revisions: + added date structure for construtive data structure application(s)

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-// declaration of month array
+//  date structure
+struct date
+{
+    int day, month, year;
+};
 
+// declaration of month array
 int months[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
 // checking leap year
-
 int checkLeap(int year) {
     if((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0))) {
         return 29;
@@ -16,17 +20,16 @@ int checkLeap(int year) {
 }
 
 // returns days number
-
-int day_Number(int day, int month, int year) {
+int day_Number(struct date d) {
     int dayNumber = 0, i;
-    months[2] = checkLeap(year);
-    for (i = 1; i < month; i++) {
+    months[2] = checkLeap(d.year);
+    for (i = 1; i < d.month; i++) {
         dayNumber += months[i];
     }
-    dayNumber += day;
-    
-    for (i = 1; i < year; i++) {
-        if (checkLeap(year) == 29) {
+    dayNumber += d.day;
+
+    for (i = 1; i < d.year; i++) {
+        if (checkLeap(d.year) == 29) {
             dayNumber += 366;
         }
         else {
@@ -34,41 +37,42 @@ int day_Number(int day, int month, int year) {
         }
     }
     return dayNumber;
-    
 }
 
-// display in year, month and days format
+// calculate in year, month and days format and return as a date structure
+struct date dayDifference(struct date d1, struct date d2) {
+    int first = day_Number(d1);
+    int second = day_Number(d2);
+    int diff = abs(first - second); // finding absolute date difference
 
-void displayFormat(int days) {
+    struct date d;
     int month, year;
-    year = days / 365;
-    days %= 365;
-    month = days / 30;
-    days %= 30;
-    
-    printf("Difference is %d years, %d months, %d days\n", year, month, days);
-    
-    
+    d.year = diff / 365;
+    diff %= 365;
+    d.month = diff / 30;
+    diff %= 30;
+    d.day = diff;
+
+    return d;
 }
 
+//  main function
 int main() {
-    int day1, month1, year1, day2, year2, month2;
-    
+    struct date d1, d2;
+
     // input first date
-    
     printf("Enter First date: ");
-    scanf("%d%d%d", &day1, &month1, &year1);
-    
+    scanf("%d%d%d", &d1.day, &d1.month, &d1.year);
+
     //  input second date
-    
     printf("Enter Second date: ");
-    scanf("%d%d%d", &day2, &month2, &year2);
-    
-    int first = day_Number(day1, month1, year1);
-    int second = day_Number(day2, month2, year2);
-    int difference = abs(first - second); // finding absolute date difference
-    
-    displayFormat(difference);
+    scanf("%d%d%d", &d2.day, &d2.month, &d2.year);
+
+    //  calculating difference
+    struct date diff;
+    diff = dayDifference(d1, d2);
+
+    printf("\nDifference is %d years, %d months, %d days\n", diff.year, diff.month, diff.day);
+
     return 0;
-    
 }
